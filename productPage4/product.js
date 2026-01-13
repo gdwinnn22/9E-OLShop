1cceb0c106096a346997f14c2780edbde04c3d0f
gdwinnn22 <gagah.godwin2605@gmail.com>
2026-01-14 01:41:55 +0700

    cp

---
+++ productPage4/product.js
@@ -0,0 +1,39 @@
+const homeTrigger = document.getElementById("homeTrigger");
+const homeAnchor = document.getElementById("textArea");
+
+// ==========================
+// SVG HOVER
+// ==========================
+homeTrigger.addEventListener("mouseenter", () => {
+  homeTrigger.classList.add("is-hover");
+});
+
+homeTrigger.addEventListener("mouseleave", () => {
+  if (!homeTrigger.classList.contains("is-hold")) {
+    homeTrigger.classList.remove("is-hover");
+  }
+});
+
+// ==========================
+// ANCHOR TAHAN ANIMASI
+// ==========================
+homeAnchor.addEventListener("mouseenter", () => {
+  homeTrigger.classList.add("is-hold");
+});
+
+homeAnchor.addEventListener("mouseleave", () => {
+  homeTrigger.classList.remove("is-hover");
+  homeTrigger.classList.remove("is-hold");
+});
+
+// ==========================
+// ACCESSIBILITY KEYBOARD
+// ==========================
+homeAnchor.addEventListener("focus", () => {
+  homeTrigger.classList.add("is-hold");
+});
+
+homeAnchor.addEventListener("blur", () => {
+  homeTrigger.classList.remove("is-hold");
+  homeTrigger.classList.remove("is-hover");
+});
