68bd532a4319989d0593bf3770481d832dc48e9b
gdwinnn22 <gagah.godwin2605@gmail.com>
2026-01-14 06:25:07 +0700

    push file1

---
+++ productPage2/script.js
@@ -0,0 +1,118 @@
+// main webpage btn interaction
+
+const menuButton = document.getElementById("menu-btn");
+const navbar = document.getElementById("nav-popup");
+const navCloseButton = document.getElementById("close-btn")
+
+menuButton.onclick = () => navbar.classList.toggle("is-active")
+navCloseButton.onclick = () => navbar.classList.remove('is-active')
+
+document.addEventListener("click", (event) => {
+  const handleOutsideClick = 
+  menuButton.contains(event.target) || navbar.contains(event.target) || navCloseButton.contains(event.target);
+
+  if (!handleOutsideClick) {
+    navbar.classList.remove("is-active")
+  }
+})
+
+// copy link auto
+
+const copyElements = document.querySelectorAll("#copy-text");
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text);
+}
+
+copyElements.forEach(element => {
+  element.addEventListener("click", () => {
+    const text = element.dataset.copy;
+    copyToClipboard(text);
+    alert("Link Copied!")
+  });
+});
+
+// animasi cart
+
+const trigger = document.getElementById("cart-trigger");
+const anchor = document.getElementById("cart-text");
+
+//SVG hover
+trigger.addEventListener("mouseenter", () => {
+  trigger.classList.add("is-hover");
+});
+
+trigger.addEventListener("mouseleave", () => {
+  if (!trigger.classList.contains("is-hold")) {
+    trigger.classList.remove("is-hover");
+  }
+});
+
+// anchor tahan animasi
+
+anchor.addEventListener("mouseenter", () => {
+  trigger.classList.add("is-hold");
+});
+
+anchor.addEventListener("mouseleave", () => {
+  trigger.classList.remove("is-hover");
+  trigger.classList.remove("is-hold");
+});
+
+// accessibility keyboard
+
+anchor.addEventListener("focus", () => {
+  trigger.classList.add("is-hold");
+});
+
+anchor.addEventListener("blur", () => {
+  trigger.classList.remove("is-hold");
+  trigger.classList.remove("is-hover");
+});
+
+//logo kiri
+
+const triggerLeft = document.getElementById("side-trigger");
+const anchorLeft = document.getElementById("side-text");
+
+//SVG hover
+triggerLeft.addEventListener("mouseenter", () => {
+  triggerLeft.classList.add("is-hover");
+});
+
+triggerLeft.addEventListener("mouseleave", () => {
+  if (!triggerLeft.classList.contains("is-hold")) {
+    triggerLeft.classList.remove("is-hover");
+  }
+});
+
+// anchor tahan animasi
+
+anchorLeft.addEventListener("mouseenter", () => {
+  triggerLeft.classList.add("is-hold");
+});
+
+anchorLeft.addEventListener("mouseleave", () => {
+  triggerLeft.classList.remove("is-hover");
+  triggerLeft.classList.remove("is-hold");
+});
+
+// accessibility keyboard
+
+anchorLeft.addEventListener("focus", () => {
+  triggerLeft.classList.add("is-hold");
+});
+
+anchorLeft.addEventListener("blur", () => {
+  triggerLeft.classList.remove("is-hold");
+  triggerLeft.classList.remove("is-hover");
+});
+
+const holdButton = document.querySelectorAll(".size-btn");
+
+holdButton.forEach(el => {
+  el.addEventListener("click", () => {
+    alert("toggle hold!")
+  })
+});
+
